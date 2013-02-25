c546bbf6d967598293a86f0cea1077dae19a0885
Hans de Graaff <hans@degraaff.org>
2013-02-25 18:55:59 +0100

    Now available in the gentoo tree.

--- app-emacs/scss-mode/files/50scss-mode-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; scss-mode site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
-(autoload 'scss-mode "scss-mode" "Mode for editing SCSS stylesheets")
