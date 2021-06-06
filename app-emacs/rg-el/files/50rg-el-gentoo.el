67af6c6ee1af52265b9177d60308815116aacd32
Hans de Graaff <hans@degraaff.org>
2021-06-06 07:54:56 +0200

    app-emacs/rg-el: add 2.0.3
    Package-Manager: Portage-3.0.18, Repoman-3.0.2
    Signed-off-by: Hans de Graaff <hans@degraaff.org>

--- app-emacs/rg-el/files/50rg-el-gentoo.el
+++ app-emacs/rg-el/files/50rg-el-gentoo.el
@@ -2,6 +2,9 @@
 ;;; rg.el site-lisp configuration
 
 (add-to-list 'load-path "@SITELISP@")
+(autoload 'rg-enable-default-bindings "rg.el" "" t)
+(autoload 'rg-use-old-defaults "rg.el" "" t)
+(autoload 'rg-define-search "rg.el" "" t)
 (autoload 'rg-project "rg.el" "" t)
 (autoload 'rg-dwim-project-dir "rg.el" "" t)
 (autoload 'rg-dwim-current-dir "rg.el" "" t)
