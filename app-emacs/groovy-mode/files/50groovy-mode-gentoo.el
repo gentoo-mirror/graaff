a83071eeec09bf36b6b8f68acd38ebe058be1352
Hans de Graaff <hans@degraaff.org>
2023-04-02 09:07:33 +0200

    app-emacs/groovy-mode: in ::gentoo as groovy-emacs-modes
    Signed-off-by: Hans de Graaff <hans@degraaff.org>

--- app-emacs/groovy-mode/files/50groovy-mode-gentoo.el
+++
@@ -1,6 +0,0 @@
-
-;;; groovy-mode site-lisp configuration
-
-(add-to-list 'load-path "@SITELISP@")
-(add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
-(autoload 'groovy-mode "groovy-mode" "Mode for editing Groovy")
