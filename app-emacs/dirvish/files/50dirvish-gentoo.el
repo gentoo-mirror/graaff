816c11e75154b62dbfd9762a092bb0899c4e681c
Hans de Graaff <hans@degraaff.org>
2023-07-05 12:13:44 +0200

    app-emacs/dirvish: add missing site file
    Signed-off-by: Hans de Graaff <hans@degraaff.org>

---
+++ app-emacs/dirvish/files/50dirvish-gentoo.el
@@ -0,0 +1,8 @@
+
+;;; rg.el site-lisp configuration
+
+(add-to-list 'load-path "@SITELISP@")
+
+(autoload 'dirvish-override-dired-mode "dirvish" "" t)
+(autoload 'dirvish "dirvish" "" t)
+(autoload 'dirvis-dwim "dirvish" "" t)
