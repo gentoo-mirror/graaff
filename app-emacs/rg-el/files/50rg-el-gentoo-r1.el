776f8fefe3c3a8e30b4221e0e7e573829fad0862
Hans de Graaff <hans@degraaff.org>
2023-09-07 21:43:46 +0200

    app-emacs/rg-el: add 2.3.0
    Signed-off-by: Hans de Graaff <hans@degraaff.org>

---
+++ app-emacs/rg-el/files/50rg-el-gentoo-r1.el
@@ -0,0 +1,15 @@
+(add-to-list 'load-path "@SITELISP@")
+
+(autoload 'rg-menu "rg-menu.el" "" t)
+(autoload 'rg-enable-menu "rg-menu.el" "" t)
+
+(autoload 'rg-enable-default-bindings "rg.el" "" t)
+(autoload 'rg-use-old-defaults "rg.el" "" t)
+(autoload 'rg-define-search "rg.el" "" t)
+(autoload 'rg-project "rg.el" "" t)
+(autoload 'rg-dwim-project-dir "rg.el" "" t)
+(autoload 'rg-dwim-current-dir "rg.el" "" t)
+(autoload 'rg-dwim-current-file "rg.el" "" t)
+(autoload 'rg-dwim "rg.el" "" t)
+(autoload 'rg-literal "rg.el" "" t)
+(autoload 'rg "rg.el" "" t)
